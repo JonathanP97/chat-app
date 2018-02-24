c46602eb3a6c45e31cb2d31469d9c7b965fe19ff
Jonathan Plata <jonathan_plata@ymail.com>
2018-02-24 03:46:00 -0800

    setup

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ var io = require('socket.io')(http);
 
 io.on('connection', function(socket) {
 	console.log('user connected');
+
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
-	})
+	});
 
-	socket.on('chat message', function(msg) {
-		console.log('msg:' + msg);
-	})
-})
+	socket.on('new-user', function(name) {
+		io.emit('new-user', name);
+	});
+
+	socket.on('msg', function(user) {
+		var message = user.msg
+		console.log('test ====== ' +message);
+		console.log('msg: ' + user.msg);
+		io.emit('msg', user.name + ":  " + user.msg);
+	});
+});
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function() {
-	console.log('On port 3000');
+http.listen(3500, function() {
+	console.log('On port 3500');
 });

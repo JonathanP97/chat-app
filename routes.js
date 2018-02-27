93dfe8e98f7bace20e6a72c0dddec4ffd3253136
Jonathan Plata <jonathan_plata@ymail.com>
2018-02-26 16:45:40 -0800

    direct message page setup

--- routes.js
+++ routes.js
@@ -5,12 +5,28 @@ module.exports = function(app, passport) {
 		res.sendFile(__dirname + '/public/chat.html');
 	});
 
+	app.get('/home', function(req, res) {
+		res.sendFile(__dirname + '/public/home.html');
+	});
+
+
 	app.get('/', function(req, res) {
 		res.sendFile(__dirname + '/public/login.html');
 	});
 
+
 	app.get('/api/users', function(req, res) {
 		db.User.find({}).then(function(user) {
+			res.json(user);
+		}).catch(function(err) {
+			res.json(err);
+		});
+	});
+
+	app.get('/api/users/:user', function(req, res) {
+		var u = req.params.user
+		console.log(u)
+		db.User.find({username: u}).then(function(user) {
 			res.json(user);
 		}).catch(function(err) {
 			res.json(err);
